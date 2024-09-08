24b0b7ccbc9fdb2916cdf2a429331b3475e59bbe
Minh <lightstormlxag93@gmail.com>
2024-09-09 02:53:12 +0700

    update token

--- src/services/config.js
+++ src/services/config.js
@@ -4,7 +4,7 @@ export const http = axios.create({
   baseURL: "https://movienew.cybersoft.edu.vn/api",
   headers: {
     tokenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODgwMDAwMCIsIm5iZiI6MTY5ODY4NTIwMCwiZXhwIjoxNzI1Mjk2NDAwfQ.CPY1b9IiMcklQZ9hjqIzrdiOlQ5YnV4VpzGu_yZr7G0",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODgwMDAwMCIsImlhdCI6MTcyNTgyNDcyMiwiZXhwIjoxNzI2MjU2NzIyfQ.IOTy9CbaV5LVfEYarlNvGtn_v_PU-H_DIcFsm-E1lEE",
   },
   timeout: 30000,
 });
